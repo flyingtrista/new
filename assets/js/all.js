b964a6e6eb83540e7fef99fb668246c4d60750b1
dora <trista.fly@gmail.com>
2022-09-13 10:11:43 +0800

    Update 2022-09-13T02:11:39.912Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,7 @@ console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
 });
+var a = 'Hello';
+var b = Hello;
+console.log(a, b);
 //# sourceMappingURL=all.js.map
